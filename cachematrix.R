5043e2479fe6f4b39e96aa5b965e8fc7537769b9
Hanifkhademi <138097048+Hanifkhademi@users.noreply.github.com>
2023-08-30 15:09:49 +0330

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+# InvertMatrix ------------------------------------------------------------
 
-## Write a short comment describing this function
+# Function to create a special matrix object with caching
+makeCacheMatrix <- function() {
+  # Initialize the matrix and inverse variables
+  matrix <- NULL
+  inverse <- NULL
+  
+  # Function to set the matrix
+  set <- function(mat) {
+    matrix <<- mat
+    inverse <<- NULL  # Reset the inverse cache when matrix changes
+  }
+  
+  # Function to get the matrix
+  get <- function() {
+    matrix
+  }
+  
+  # Function to set the inverse of the matrix
+  setInverse <- function(inv) {
+    inverse <<- inv
+  }
+  
+  # Function to get the inverse of the matrix
+  getInverse <- function() {
+    inverse
+  }
+  
+  # Return a list of functions
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# Function to compute and cache the inverse of a matrix
+cacheSolve <- function(cacheMatrix) {
+  # Check if the inverse is already cached
+  if (!is.null(cacheMatrix$getInverse())) {
+    message("Retrieving cached inverse")
+    return(cacheMatrix$getInverse())
+  }
+  
+  # Compute the inverse
+  matrix <- cacheMatrix$get()
+  inv <- solve(matrix)
+  
+  # Cache the inverse
+  cacheMatrix$setInverse(inv)
+  
+  # Return the inverse
+  inv
 }
 
 
-## Write a short comment describing this function
+# Create a matrix and cache its inverse
+myMatrix <- makeCacheMatrix()
+myMatrix$set(matrix(c(1, 2, 3, 4), nrow = 2))
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# Compute the inverse (first time, computes and caches)
+cacheSolve(myMatrix)
+
+# Retrieve the cached inverse (second time, retrieves from cache)
+cacheSolve(myMatrix)
+
+
+
